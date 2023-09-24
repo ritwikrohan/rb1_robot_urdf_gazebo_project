e4db560f23f095a6e0778f0f101dca7087af453f
Ritwik Rohan <ritwikrohan7@gmail.com>
2023-09-24 22:45:41 +0000

    Checkpoint 2 reeval

--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -2,6 +2,8 @@
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
+#include <string>
+#include <tf/tf.h>
 
 class RotateService {
 
@@ -14,7 +16,9 @@ public:
   double initial_yaw = 0.0;
   double current_yaw;
   double target_yaw;
+  double target_yaw_n;
   double delta_yaw; 
+  double delta_yaw_n;
 
   RotateService() {
     
@@ -24,14 +28,22 @@ public:
     ROS_INFO("Rotate service is ready.");
     vel_pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
   }
+  
+  double normalizeAngle(double angle) {
+    while (angle > M_PI) {
+        angle -= 2.0 * M_PI;
+    }
+    while (angle < -M_PI) {
+        angle += 2.0 * M_PI;
+    }
+    return angle;
+  }
 
   void rotateStart() {
-    if (delta_yaw > 0) {
-      vel_msg.angular.z = 0.2; 
-      ROS_INFO("Rotating counterclockwise...");
+    if (delta_yaw_n > 0) {
+      vel_msg.angular.z = 0.5; 
     } else {
-      vel_msg.angular.z = -0.2; 
-      ROS_INFO("Rotating clockwise...");
+      vel_msg.angular.z = -0.5; 
     }
     vel_pub.publish(vel_msg);
   }
@@ -45,16 +57,19 @@ public:
                       my_rb1_ros::Rotate::Response &res) 
   {
     ROS_INFO("The Service has been called");
-
-    
     delta_yaw = req.degrees * M_PI / 180.0;
-    target_yaw = initial_yaw + delta_yaw;
-
+    delta_yaw_n = normalizeAngle(delta_yaw);
     
-    while (std::abs(current_yaw - target_yaw) >= 0.05) {
-      rotateStart();
-      ROS_INFO("Rotation in progress");
+    target_yaw = initial_yaw + delta_yaw_n;
+    
+    target_yaw_n = normalizeAngle(target_yaw);
+    
+    // ros::Rate rate(10);
+
+    while (std::abs(current_yaw - target_yaw_n) >= 0.08 && std::abs(delta_yaw)<=((2*M_PI)-0.0872665)) {
       ros::spinOnce();
+      rotateStart();
+    //   rate.sleep();
     }
 
     rotateStop();
@@ -64,15 +79,16 @@ public:
     return true;
   }
 
-  void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
+    void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
     double qx = msg->pose.pose.orientation.x;
     double qy = msg->pose.pose.orientation.y;
     double qz = msg->pose.pose.orientation.z;
     double qw = msg->pose.pose.orientation.w;
-
-    current_yaw =
+    double unnormalized_yaw =
         atan2(2.0 * (qw * qz + qx * qy), qw * qw + qx * qx - qy * qy - qz * qz);
-  }
+    current_yaw = normalizeAngle(unnormalized_yaw);
+    }
+
 };
 
 int main(int argc, char **argv) {
